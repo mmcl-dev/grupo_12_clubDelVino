d62d2303d7df69ce61d3b950d8cc0c3d4e59145d
Gabriel-Eduardo-Ruffo <gon.near@gmail.com>
2020-12-21 19:32:47 -0300

    agregado de html y css de carrito de compra, modificacion en index.html

--- site/src/app.js
+++ site/src/app.js
@@ -9,6 +9,17 @@ app.use (express.static(publicPath))
 app.listen(3030, ()  => console.log('Ya estoy escuchando...'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));//muestra el home
 });
 
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/index.html'));//muestra el home
+});
+
+app.get('/productCart', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'));//muestra la pagina de listado de productos, el carrito
+});
+
+app.get('*',(req,res)=>{
+    res.send('404 no se encontro la pagina buscada');//cualquier pagina que no se encuentre
+});
