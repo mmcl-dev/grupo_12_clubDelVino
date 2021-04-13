45f3c3ad66f6b7951c047387693eb8467371e1b4
NearLawliet <gon.near@gmail.com>
2021-04-12 22:02:10 -0300

    middleware de validaciones de permisos administrador o usuario. SIN terminar

--- site/src/middlewares/authMiddleware.js
+++ site/src/middlewares/authMiddleware.js
@@ -1,3 +1,13 @@
 //verificar si el usuario esta logueado por medio de session
 //redirijir al home si no esta logueado con permiso para el CRUD
 
+const db = require("../../../database/models");
+
+module.exports = (req, res, next) => {
+    if (!req.session.user) {
+        //hacer validaciones para saber si es administrador o usuario comun. esto sirve para saber si puede o no editar un producto, o crear producto
+        //este middleware hay que agregarlo a las rutas que correspondan, (que necesite ser administrador)
+        return res.redirect('/home');
+    }
+    next();
+};
